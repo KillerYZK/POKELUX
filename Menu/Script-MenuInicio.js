573f8fbd04fa0d181f3e3eeea4c403c34ebdea17
Eduardo-Munguia <lalo.munro@gmail.com>
2026-04-14 10:57:36 -0600

    Header-Funcional
    Hewwooo!! ^w^, ya tenemos headers chat<3 ah y tmb agregue un styles a cada archivo html por separado UwU

--- Menu/Script-MenuInicio.js
+++ Menu/Script-MenuInicio.js
@@ -1,11 +1,11 @@
-document.getElementById("Lista").addEventListener("click", () => {
-  window.location.href = "Lista.html";
-});
-
 document.getElementById("Combate").addEventListener("click", () => {
   window.location.href = "Combate.html";
 });
 
 document.getElementById("Equipo").addEventListener("click", () => {
   window.location.href = "Equipo.html";
+});
+
+document.getElementById("Inicio").addEventListener("click", () => {
+  window.location.href = "Menu-Inicio.html";
 });
