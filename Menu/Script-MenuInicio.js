e7814f2c09550f87fc6ad299dc8c1433bffcf000
Eduardo-Munguia <lalo.munro@gmail.com>
2026-04-14 07:54:22 -0600

    Creacion-Paginas
    Holiii <3, en esta lo que hice fue hacer todas las paginas de html y vincularlas con javascript >w<

---
+++ Menu/Script-MenuInicio.js
@@ -0,0 +1,11 @@
+document.getElementById("Lista").addEventListener("click", () => {
+  window.location.href = "Lista.html";
+});
+
+document.getElementById("Combate").addEventListener("click", () => {
+  window.location.href = "Combate.html";
+});
+
+document.getElementById("Equipo").addEventListener("click", () => {
+  window.location.href = "Equipo.html";
+});
