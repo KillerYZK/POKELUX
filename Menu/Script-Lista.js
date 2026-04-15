d09e453de007139c634f5ee43730e6f1364fa30c
Eduardo-Munguia <lalo.munro@gmail.com>
2026-04-14 18:32:39 -0600

    Intento-en-Progreso
    Intenté hacer una barra de busqueda que te vaya sugiriendo los pokemons con nombres similares según los vas escribiendo pero de momento no pude

--- Menu/Script-Lista.js
+++ Menu/Script-Lista.js
@@ -1,3 +1,4 @@
+// Codigo de la paginación
 let paginaActual = 1;
 
 document.getElementById("boton-siguiente").addEventListener("click", () => {
@@ -14,6 +15,19 @@ document.getElementById("boton-anterior").addEventListener("click", () => {
   }
 });
 
+document.getElementById("boton-inicio").addEventListener("click", () => {
+  paginaActual = 1;
+  document.getElementById("cartas-pokemon").innerHTML = "";
+  obtenerPokemon();
+});
+
+document.getElementById("boton-final").addEventListener("click", () => {
+  paginaActual = 52;
+  document.getElementById("cartas-pokemon").innerHTML = "";
+  obtenerPokemon();
+});
+
+//Listado de pokemons
 async function obtenerPokemon() {
   const listado = await fetch(
     `https://pokeapi.co/api/v2/pokemon?limit=20&offset=${(paginaActual - 1) * 20}`,
@@ -23,6 +37,7 @@ async function obtenerPokemon() {
   listadojson.results.forEach((pokemon) => {
     const cartapokemon = document.createElement("div");
     const idPokemon = pokemon.url.split("/")[6];
+    if (idPokemon > 1025) return; // Le tengo que preguntar a Isaac si vamos a usar megaevoluciones, gmax, etc. En casi de que si, borrar esta linea
     cartapokemon.innerHTML = `
       <img
         class="imagen-pokemon"
@@ -36,3 +51,30 @@ async function obtenerPokemon() {
 }
 
 obtenerPokemon();
+
+// Codigo de la barra de busqueda (esto sigue en progreso, todo esto esta sujeto a cambios)
+async function buscarPokemon() {
+  const input = document.getElementById("input-busqueda");
+  const botonBuscar = document.getElementById("boton-busqueda");
+
+  input.addEventListener("input", async () => {
+    const valor = input.value.toLowerCase();
+    if (valor !== "") {
+      const busqueda = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${valor}`,
+      );
+      if (busqueda.ok) {
+        const pokemon = await busqueda.json();
+        document.getElementById("cartas-pokemon").innerHTML = "";
+        document
+          .getElementById("cartas-pokemon")
+          .appendChild(createPokemonCard(pokemon));
+      } else {
+        obtenerPokemon();
+        return;
+      }
+    }
+  });
+}
+
+buscarPokemon();
